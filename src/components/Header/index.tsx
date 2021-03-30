4ca5cccbdc2ac37f4561d4abc05b751faecd47bb
IgorTargino <igortargino01@gmail.com>
2021-03-30 18:08:59 -0300

    feat: header and tool

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+
+import { AiOutlinePlus } from 'react-icons/ai';
+import styles from './styles.module.scss';
+
+function Header() {
+  return (
+    <header className={styles.container}>
+
+      <div className={styles.title}>
+        <h1>VUTTR</h1>
+        <h2>Very Useful Tools to Remember</h2>
+      </div>
+
+      <div className={styles.toolbar}>
+
+        <div className={styles.search}>
+          <input 
+            type="search" 
+            placeholder="Digite o que está procurando..." 
+            className={styles.searchBar}
+          />
+          <section>
+            <input type="checkbox"/>
+            <span>search in tags only</span>
+          </section>
+        </div >
+
+        <button className={styles.button}>
+          <AiOutlinePlus size={20}/>
+          Add
+        </button>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Tool/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.scss";
+
+function Tool() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <p>Tool</p>
+        <button className={styles.button}>
+          <img src="icon-close.svg" alt="close" />
+          remove
+        </button>
+      </div>
+      <div className={styles.data}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam enim
+          necessitatibus doloremque architecto unde, odit nobis dolorem fuga
+        </p>
+        <p>
+          <strong>#tags #tag2 #tag3 #tag4</strong>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Tool;
