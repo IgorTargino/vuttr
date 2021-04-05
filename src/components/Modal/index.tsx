dea1dc288c78cd29a1efa126ccd68c9659286d1c
IgorTargino <igortargino01@gmail.com>
2021-04-04 23:16:49 -0300

    update: style modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,13 +1,51 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 function Modal() {
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        Teste
+        <form action="">
+          <fieldset>
+            <legend>Add new tool</legend>
+
+            <div>
+              <label htmlFor="tool-name">Tool name</label>
+              <br />
+              <input type="text" id="tool-name" placeholder="Hotel" />
+            </div>
+
+            <div>
+              <label htmlFor="tool-link">Tool link</label>
+              <br />
+              <input
+                type="url"
+                id="tool-link"
+                placeholder="https://github/typicode/hotel"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="tool-description">Tool description</label>
+              <br />
+              <textarea name="description" id="description" ></textarea>
+            </div>
+
+            <div>
+              <label htmlFor="tags">Tags</label>
+              <br />
+              <input
+                type="text"
+                id="tags"
+                placeholder="node organizing webapps domain developer https proxy"
+              />
+            </div>
+
+            <input type="submit" value="Add tool" />
+          </fieldset>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Modal;
