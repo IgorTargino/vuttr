e94b90bff7aedd931e0238bc5229406a6db5cbde
IgorTargino <igortargino01@gmail.com>
2021-04-05 12:05:50 -0300

    update: css modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,38 +6,43 @@ function Modal() {
       <div className={styles.container}>
         <form action="">
           <fieldset>
-            <legend>Add new tool</legend>
-
+            <header>
+              <legend>Add new tool</legend>
+              <button>
+                <img src="icon-close.svg" alt="" />
+              </button>
+            </header>
             <div>
-              <label htmlFor="tool-name">Tool name</label>
-              <br />
-              <input type="text" id="tool-name" placeholder="Hotel" />
+              <label>
+                Tool name
+                <input type="text" id="tool-name" placeholder="Hotel" />
+              </label>
             </div>
-
             <div>
-              <label htmlFor="tool-link">Tool link</label>
-              <br />
-              <input
-                type="url"
-                id="tool-link"
-                placeholder="https://github/typicode/hotel"
-              />
+              <label>
+                Tool link
+                <input
+                  type="url"
+                  id="tool-link"
+                  placeholder="https://github/typicode/hotel"
+                />
+              </label>
             </div>
-
             <div>
-              <label htmlFor="tool-description">Tool description</label>
-              <br />
-              <textarea name="description" id="description" ></textarea>
+              <label>
+                Tool description
+                <textarea name="description" id="description"></textarea>
+              </label>
             </div>
-
             <div>
-              <label htmlFor="tags">Tags</label>
-              <br />
-              <input
-                type="text"
-                id="tags"
-                placeholder="node organizing webapps domain developer https proxy"
-              />
+              <label>
+                Tags
+                <input
+                  type="text"
+                  id="tags"
+                  placeholder="node organizing webapps domain developer https proxy"
+                />
+              </label>
             </div>
 
             <input type="submit" value="Add tool" />
